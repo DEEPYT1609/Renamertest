89dc20ae4837032c6151aa6094c4a1efea289108
DEEPYT1609 <132783089+DEEPYT1609@users.noreply.github.com>
2023-05-12 12:34:47 +0530

    Update web_support.py

--- plugins/web_support.py
+++ plugins/web_support.py
@@ -1,3 +1,10 @@
+from flask import Flask
+from time import sleep, time
+from psutil import boot_time, disk_usage, net_io_counters
+from subprocess import check_output
+from os import path as ospath
+app = Flask(__name__)
+botStartTime = time()
 from aiohttp import web
 
 routes = web.RouteTableDef()
@@ -5,6 +12,14 @@ routes = web.RouteTableDef()
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
     return web.json_response("LazyDeveloper")
+
+@route.get('/status', allow_head=True)
+async def status(request):
+    bot_uptime = time() - botStartTime
+    uptime = time() - boot_time()
+    sent = net_io_counters().bytes_sent
+    recv = net_io_counters().bytes_recv
+    return web.json_response('uptime': uptime,'on_time': bot_uptime,'free_disk': disk_usage('.').free,'total_disk': disk_usage('.').total,'network': {'sent': sent,'recv': recv,},)
 
 
 async def web_server():
